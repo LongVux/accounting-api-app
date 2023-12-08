ce17959f3c07fcb08100197c03314e39b1fd0fb5
vunl <longvux1911@gmail.com>
2023-12-08 10:20:07 +0700

    fixing get matching bill request

--- src/main/java/com/outwork/accountingapiapp/models/payload/requests/GetMatchingBillsRequest.java
+++ src/main/java/com/outwork/accountingapiapp/models/payload/requests/GetMatchingBillsRequest.java
@@ -2,6 +2,7 @@ package com.outwork.accountingapiapp.models.payload.requests;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.UUID;
@@ -14,9 +15,11 @@ public class GetMatchingBillsRequest {
     private UUID posId;
 
     @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date fromCreatedDate;
 
     @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date toCreatedDate;
 
     @NotNull
